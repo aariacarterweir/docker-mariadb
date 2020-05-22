2cc03cde4e6ccffc08986153621ef702765d9eb6
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-22 21:35:42 +1000

    tidy up how shared folders work

--- run.sh
+++ run.sh
@@ -3,23 +3,25 @@
 # Load config
 . load-config.sh
 
-# ensure global persist directory exists
-if [ ! -d "persist" ]; then
-  mkdir persist
+# ensure global shared directories exist
+if [ ! -d "shared/mysql" ]; then
+  mkdir -p shared/mysql
 fi
 
-# ensure global persist directory exists
-if [ ! -d "shared" ]; then
-  mkdir shared
-  chmod -R 755 shared
+if [ ! -d "shared/files" ]; then
+  mkdir -p shared/files
 fi
 
 # docker run command
 docker run \
   --detach \
-  --mount  type=bind,source="$(pwd)/persist/",target=/var/lib/mysql \
-  --mount  type=bind,source="$(pwd)/shared/",target=/shared \
-  --env-file config.txt \
+  --tty \
+  --mount  type=bind,source="$(pwd)/shared/mysql/",target=/var/lib/mysql \
+  --mount  type=bind,source="$(pwd)/shared/files/",target=/shared \
+  --env MARIADB_ALLOW_EMPTY_PASSWORD="$MARIADB_ALLOW_EMPTY_PASSWORD" \
+  --env MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" \
+  --env MARIADB_USER="$MARIADB_USER" \
+  --env MARIADB_PASSWORD="$MARIADB_PASSWORD" \
   --name "$CONTAINER_NAME" \
   -p "$LOCAL_PORT":"$CONTAINER_PORT" \
   "$IMAGE_NAME"
