2dccd2ac0016ac5e617c7233ab2fd3857e294fb9
Aaria Carter-Weir <aaria@mxstore.com.au>
2020-05-22 12:56:11 +1000

    mount a shared folder

--- run.sh
+++ run.sh
@@ -6,13 +6,19 @@
 # ensure global persist directory exists
 if [ ! -d "persist" ]; then
   mkdir persist
-  chmod -R 755 persist
+fi
+
+# ensure global persist directory exists
+if [ ! -d "shared" ]; then
+  mkdir shared
+  chmod -R 755 shared
 fi
 
 # docker run command
 docker run \
   --detach \
   --mount  type=bind,source="$(pwd)/persist/",target=/var/lib/mysql \
+  --mount  type=bind,source="$(pwd)/shared/",target=/shared \
   --env-file config.txt \
   --name "$CONTAINER_NAME" \
   -p "$LOCAL_PORT":"$CONTAINER_PORT" \
